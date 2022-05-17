3924a8fdeb08c1a8f9cb43fc6661a3a23ae8824d
UMAIR SHABBIR <93377842+umairshabbir-83@users.noreply.github.com>
2022-05-17 23:18:56 +0500

    Add files via upload

---
+++ Backward Difference.h
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+int BackwardDifference() {
+    float x[20], y[20][20];
+    int i, j, n;
+
+    /* Input Section */
+    cout << "Enter number of data? " << endl;
+    cin >> n;
+
+    cout << "Enter data: " << endl;
+    for (i = 0; i < n; i++) {
+        cout << "X[" << i << "] = ";
+        cin >> x[i];
+        cout << "Y[" << i << "] = ";
+        cin >> y[i][0];
+    }
+
+    /* Generating Backward Difference Table */
+    for (i = 1; i < n; i++) {
+        for (j = n - 1; j > i - 1; j--) {
+            y[j][i] = y[j][i - 1] - y[j - 1][i - 1];
+        }
+    }
+
+    /* Displaying Backward Difference Table */
+    cout << endl << "\t**BACKWARD DIFFERENCE TABLE**" << endl;
+    cout << endl;
+
+    for (i = 0; i < n; i++) {
+        cout << x[i];
+        for (j = 0; j <= i; j++) {
+            cout << "\t" << y[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
